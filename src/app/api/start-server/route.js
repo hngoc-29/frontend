fd965c16d615414ebb9eaf95c501b600b632c97f
HNgoc <lmtcv1.9@gmail.com>
2025-03-12 17:08:56 +0700

    c

--- src/app/api/start-server/route.js
+++ src/app/api/start-server/route.js
@@ -1,8 +1,8 @@
-export async function POST(request) {
+export async function GET(request) {
     try {
         // Logic để khởi động máy chủ
         console.log('Khởi động máy chủ...');
-        const response = await fetch(`${process.env.URL_BACKEND}/v1/start-server`);
+        const response = await fetch(`${process.env.URL_BACKEND}/v1/start-server`, { method: 'HEAD' });
         if (!response.ok) {
             console.error('Không thể khởi động máy chủ');
             return new Response('Không thể khởi động máy chủ', { status: 500 });
--- src/components/StartServer.js
+++ src/components/StartServer.js
@@ -4,37 +4,29 @@ import { loadingContext } from '../context/Loading';
 import { useToast } from '../context/Toast';
 
 const StartServer = () => {
-    const [serverStatus, setServerStatus] = useState('stopped');
     const { setLoading } = useContext(loadingContext);
     const { addToast } = useToast();
 
     useEffect(() => {
         const handleStartServer = async () => {
-            if (serverStatus === 'stopped') {
-                setLoading(true);
-                try {
-                    const response = await fetch('/api/start-server', { method: 'POST' });
-                    if (response.ok) {
-                        setServerStatus('running');
-                        console.log('Máy chủ đã khởi động');
-                    } else {
-                        addToast({ type: 'error', title: 'Lỗi', description: 'Không thể khởi động máy chủ' });
-                        console.error('Không thể khởi động máy chủ');
-                    }
-                } catch (error) {
-                    addToast({ type: 'error', title: 'Lỗi', description: 'Lỗi khi khởi động máy chủ' });
-                    console.error('Lỗi khi khởi động máy chủ:', error);
-                } finally {
-                    setLoading(false);
+            setLoading(true);
+            try {
+                const response = await fetch('/api/start-server');
+                if (response.ok) {
+                    console.log('Máy chủ đã khởi động');
+                } else {
+                    addToast({ type: 'error', title: 'Lỗi', description: 'Không thể khởi động máy chủ' });
+                    console.error('Không thể khởi động máy chủ');
                 }
+            } catch (error) {
+                addToast({ type: 'error', title: 'Lỗi', description: 'Lỗi khi khởi động máy chủ' });
+                console.error('Lỗi khi khởi động máy chủ:', error);
+            } finally {
+                setLoading(false);
             }
         };
         const interval = setInterval(() => {
-            if (serverStatus === 'stopped') {
-                handleStartServer();
-            } else {
-                clearInterval(interval);
-            }
+            handleStartServer();
         }, 1000 * 60 * 14); // Khởi động lại máy chủ sau 14 phút
         return () => clearInterval(interval);
     }, []);
