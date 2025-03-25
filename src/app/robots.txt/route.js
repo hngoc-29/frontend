3d86e00800edc92fc044c04c08eaeb73619a772d
HNgoc <lmtcv1.9@gmail.com>
2025-03-25 15:42:56 +0700

    update robot

--- src/app/robots.txt/route.js
+++ src/app/robots.txt/route.js
@@ -2,7 +2,6 @@ export function GET() {
     return new Response(
         `User-agent: *
         Allow: /
-        Disallow: /quan-ly
         Sitemap: ${process.env.BASE_URL || "https://next-app-music.vercel.app"}/sitemap.xml`,
         {
             headers: {
